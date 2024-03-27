02022ce8657575a3d841b9a49f296db52358e625
Kelvin Eduful <keduful@st.ug.edu.gh>
2024-03-27 10:28:11 +0000

    initial commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:interwise/main.dart';
+import 'package:spacex_api/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
