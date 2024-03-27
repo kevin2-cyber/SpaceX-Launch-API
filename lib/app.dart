fa33756d5f802eacc18ff5debdc266b76184870c
Kelvin Eduful <keduful@st.ug.edu.gh>
2024-03-27 14:56:00 +0000

    setting up

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:spacex_api/home.dart';
+
+class SpaceX extends StatelessWidget {
+  const SpaceX({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
