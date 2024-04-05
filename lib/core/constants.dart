2ae28b1e6e693512685fa7c029648abcea3bc89e
Kelvin Eduful <keduful@st.ug.edu.gh>
2024-04-05 12:29:44 +0000

    search feature not working

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,6 +1,18 @@
 
+import 'package:spacex_api/model/history.dart';
+
 class AppConstants {
+  // font
   static const kFont = 'One Ui Sans';
 
+  // url
   static const kBaseUrl = 'https://spacex-production.up.railway.app/';
+
+  static List<History> kDummyText = [
+    History(id: "0", title: 'title', details: 'details', eventDateUtc: DateTime.now()),
+    History(id: "1", title: 'title', details: 'details', eventDateUtc: DateTime.now()),
+    History(id: "2", title: 'title', details: 'details', eventDateUtc: DateTime.now()),
+    History(id: "3", title: 'title', details: 'details', eventDateUtc: DateTime.now()),
+    History(id: "4", title: 'title', details: 'details', eventDateUtc: DateTime.now()),
+  ];
 }
--- lib/presentation/home.dart
+++ lib/presentation/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:spacex_api/core/constants.dart';
 import 'package:spacex_api/model/history.dart';
 import 'package:spacex_api/presentation/detail.dart';
 
@@ -21,37 +22,22 @@ class _HomeScreenState extends State<HomeScreen> {
     // fetchData();
   }
 
+  void onSearchTextChanged(String searchText) {
+    setState(() {
+      histories.where((history) =>
+              history.title.toLowerCase().contains(searchText.toLowerCase()) ||
+              history.eventDateUtc.toString().toLowerCase().contains(searchText.toLowerCase()))
+          .toList();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // body: SizedBox(
-      //   height: MediaQuery.of(context).size.height,
-      //   width: MediaQuery.of(context).size.width,
-      //   child: Center(
-      //     child: _isLoading ?
-      //     const CircularProgressIndicator() :  Padding(
-      //       padding: const EdgeInsets.all(8.0),
-      //       child: ListView.builder(
-      //         itemCount: history.length,
-      //         itemBuilder: (context, index) {
-      //           return ListTile(
-      //             title: Text(
-      //                 history[index].title,
-      //                 style: Theme.of(context).textTheme.titleLarge
-      //             ),
-      //             subtitle: Text(
-      //                 '${history[index].eventDateUtc}',
-      //                 style: Theme.of(context).textTheme.titleSmall,
-      //             ),
-      //           );
-      //         },
-      //       ),
-      //     ),
-      //   ),
-      // ),
-      body: Query(
-        options: QueryOptions(
-          document: gql("""
+      body: SafeArea(
+        child: Query(
+          options: QueryOptions(
+            document: gql("""
           query SpaceX() {
             histories() {
               id
@@ -61,43 +47,94 @@ class _HomeScreenState extends State<HomeScreen> {
             }
           }
              """),
-        ),
-        builder: (QueryResult result,
-            {VoidCallback? refetch, FetchMore? fetchMore}) {
-          if (result.hasException) {
-            return Text(result.exception.toString());
-          }
+          ),
+          builder: (QueryResult result,
+              {VoidCallback? refetch, FetchMore? fetchMore}) {
+            if (result.hasException) {
+              return Text(result.exception.toString());
+            }
 
-          if (result.isLoading) {
-            return const CircularProgressIndicator();
-          }
+            if (result.isLoading) {
+              return const CircularProgressIndicator();
+            }
 
-          // parse response into a list of history objects
-          histories = (result.data!['histories'] as List)
-              .map((historyJson) => History.fromJson(historyJson))
-              .toList();
+            // parse response into a list of history objects
+            histories = (result.data!['histories'] as List)
+                .map((historyJson) => History.fromJson(historyJson))
+                .toList();
 
-          return ListView.builder(
-            itemCount: histories.length,
-            itemBuilder: (context, index) {
-              final history = histories[index];
-              return GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => Detail(history: history)));
-                },
-                child: Card.outlined(
-                  child: ListTile(
-                    title: Text(history.title),
-                    subtitle: Text(history.eventDateUtc.toString()),
+            return SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.08,
                   ),
-                ),
-              );
-            },
-          );
-        },
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: TextField(
+                      onChanged: onSearchTextChanged,
+                      style: const TextStyle(fontSize: 16, color: Colors.white),
+                      decoration: InputDecoration(
+                        contentPadding:
+                            const EdgeInsets.symmetric(vertical: 12),
+                        hintText: "Search Histories...",
+                        hintStyle: const TextStyle(color: Colors.grey),
+                        prefixIcon: const Icon(
+                          Icons.search,
+                          color: Colors.grey,
+                        ),
+                        // fillColor: Colors.grey.shade800,
+                        filled: true,
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide:
+                              const BorderSide(color: Colors.transparent),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide:
+                              const BorderSide(color: Colors.transparent),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.02,
+                  ),
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.8,
+                    width: MediaQuery.of(context).size.width,
+                    decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(20),
+                            topLeft: Radius.circular(20))),
+                    child: ListView.builder(
+                      itemCount: histories.length,
+                      itemBuilder: (context, index) {
+                        final history = histories[index];
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        Detail(history: history)));
+                          },
+                          child: Card.outlined(
+                            child: ListTile(
+                              title: Text(history.title),
+                              subtitle: Text(history.eventDateUtc.toString()),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
